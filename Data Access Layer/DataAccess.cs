90e58430b8c6811aa38a43199dcf8824334490eb
15-30850-3 <15-30850-3@student.aiub.edu>
2021-04-19 01:40:36 +0600

    Add project files.

---
+++ Data Access Layer/DataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DigitalPhotoDiary.DataAccessLayer
+{
+    class DataAccess
+    {
+        protected SqlConnection connection;
+        protected SqlCommand command;
+        public DataAccess() {
+            try
+            {
+                this.connection = new SqlConnection(ConfigurationManager.ConnectionStrings["PhotoDiaryDb"].ConnectionString);
+                this.connection.Open();
+            }
+            catch (Exception exp) { }
+        }
+
+        public SqlDataReader GetData(string sql) {
+            this.command = new SqlCommand(sql, connection);
+            return this.command.ExecuteReader();
+        }
+
+        public int ExecuteQuery(string sql)
+        {
+            this.command = new SqlCommand(sql, connection);
+            return this.command.ExecuteNonQuery();
+        }
+
+        /*~DataAccess() {
+            this.connection.Close();
+        }*/
+    }
+}
